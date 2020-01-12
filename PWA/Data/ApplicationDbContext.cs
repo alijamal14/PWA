59c4dd427f7a664994ec24dcbac878b66ec717c1
Ali Jamal <alijamal14@live.com>
2020-01-12 13:50:00 +0500

    Add project files.

---
+++ PWA/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PWA.Models;
+
+namespace PWA.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Item> Items { get; set; }
+    }
+}
